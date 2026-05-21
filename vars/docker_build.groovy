6e2fc6b0d3ba3b125a7428acfb891edd703d122e
Sachin Tomar <35167656+sachin0010@users.noreply.github.com>
2026-05-21 15:20:48 +0530

    Fix function definition syntax in docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,4 @@
-def call(String ProjectName, String ImageTag, String DockerHubUser) {
+// Define function
+def call(String ProjectName, String ImageTag, String DockerHubUser){
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
